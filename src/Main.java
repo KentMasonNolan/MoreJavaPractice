c2f2b239a7e8974c30d35b00d997f00495e59e3f
Kent Nolan <kent.nolan@gmail.com>
2023-02-14 13:31:49 +1300

    second commit from intelliJ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello test!");
+    public static void main(String[] args) {
+
+        String world = new String("Hello World!");
+        System.out.println(world);
+
+        int x;
+        x = 5;
+        System.out.println(x);
+
+        Scanner scanner = new Scanner(System.in);
+        String y = scanner.nextLine();
+        System.out.println(y);
+        int h = scanner.nextInt();
+        System.out.println(h);
+        int g = h + x;
+        System.out.println(g);
     }
 }
