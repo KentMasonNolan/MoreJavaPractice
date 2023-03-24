b65d59fc28fb1ac18004933738f362b42cbcbc0d
kent <kent.nolan@gmail.com>
2023-03-24 13:04:35 +1300

    Basic loop for product selection

--- src/Assignment1/OnlineShop.java
+++ src/Assignment1/OnlineShop.java
@@ -1,4 +1,38 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class OnlineShop {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Test");
+
+        do{
+
+            System.out.println("\nPlease pick one of the following");
+            System.out.println("A. Display inventory");
+            System.out.println("B. Rate Product");
+            System.out.println("C. Exit");
+
+            int menu = input.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("Case 1");
+                    break;
+                case 2:
+                    System.out.println("Case 2");
+                    break;
+                case 3:
+                    System.out.println("Case 3");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("PLease pick a real option, you dick");
+            }
+
+
+        }while (true);
+    }
 }
