f3d43075b394a7abd6a2ce92f403926a2d4ed713
kent <kent.nolan@gmail.com>
2023-03-24 13:44:34 +1300

    Created product object

--- src/Assignment1/Product.java
+++ src/Assignment1/Product.java
@@ -1,4 +1,18 @@
 package Assignment1;
 
 public class Product {
+    String modelName;
+    String manufacturerName;
+    double retailPrice;
+    double reliabilityRating;
+    int numberOfConsumersRated;
+
+
+public Product(String modelName, String manufacturerName, double retailPrice) {
+        this.modelName = modelName;
+        this.manufacturerName = manufacturerName;
+        this.retailPrice =retailPrice;
+        this.reliabilityRating = 0;
+        this.numberOfConsumersRated = 0;
+    }
 }
