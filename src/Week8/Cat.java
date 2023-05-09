94b41267da26481108d1a2751ce2df1a50a2f96b
Kent Nolan <kent.nolan@gmail.com>
2023-05-09 13:07:37 +1200

    Added Week 8

---
+++ src/Week8/Cat.java
@@ -0,0 +1,16 @@
+package Week8;
+
+public class Cat extends Animal{
+
+    public void feed(){
+        this.eat+=1;
+        this.happy+=10;
+    }
+    public Cat(){
+        System.out.println("Cat object instantiated!");
+        this.happy -=10;
+    }
+    public String toString(){
+        return "Cat has " + super.toString();
+    }
+}
