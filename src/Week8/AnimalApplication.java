cc3ebba76a7f0061b3c63414035c1d047e116539
Kent Nolan <kent.nolan@gmail.com>
2023-05-09 13:22:18 +1200

    Added Week 8

---
+++ src/Week8/AnimalApplication.java
@@ -0,0 +1,27 @@
+package Week8;
+
+import java.util.Scanner;
+
+public class AnimalApplication extends Animal{
+
+    Animal[] animals = new Animal[4];
+    int animalCount = 0;
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Pet Shop");
+
+        while (true){
+
+
+
+        }
+
+
+    }
+
+}
+
+
--- src/Week8/Dog.java
+++ src/Week8/Dog.java
@@ -2,7 +2,17 @@ package Week8;
 
 public class Dog extends Animal{
 
+    public void feed(){
+        this.eat+=5;
+        this.happy+=1;
+    }
 
+    public Dog() {
+        System.out.println("Dog object instantiated!");
+    }
 
+    public String toString(){
+        return "Dog has " + super.toString();
+    }
 }
 
