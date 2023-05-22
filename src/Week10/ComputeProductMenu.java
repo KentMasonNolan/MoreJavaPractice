dc0e2b530ceec6895b2d6150c79009bcd4400b3d
kent <kent.nolan@gmail.com>
2023-05-22 14:41:44 +1200

    Week 10 lab 9

--- src/Week10/ComputeProductMenu.java
+++ src/Week10/ComputeProductMenu.java
@@ -1,13 +1,60 @@
 package Week10;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ComputeProductMenu{
+public class ComputeProductMenu {
+
+    private static Scanner input;
+
+
+    public static void product() throws InputMismatchException {
+        try {
+            System.out.println("Welcome to the calculator.");
+            System.out.println("Enter first number:");
+            int value1 = input.nextInt();
+            System.out.println("Enter second number:");
+            int value2 = input.nextInt();
+            System.out.println("Product is: " + (value1 * value2));
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("You must input integer values");
+            System.err.println("Exceptional event: " + inputMismatchException);
+            input.next();
+        }
+    }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
 
+        System.out.println("Welcome to the calculator.");
+
+
+        while (true) {
+            System.out.println("Please select from the following:");
+            System.out.println("1. Compute product");
+            System.out.println("2. Quit");
+
+            int menu = input.nextInt();
+
+            try {
+                switch (menu){
+                    case 1:
+                        System.out.println("1");
+                        break;
+                    case 2:
+                        System.out.println("2");
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+            } catch (InputMismatchException inputMismatchException){
+                System.out.println("You must input integer values");
+                System.err.println("Exceptional event: "+inputMismatchException);
+                input.next();//remove bad input from the scanner.
+            }
+
+
+        }
     }
 }
-
