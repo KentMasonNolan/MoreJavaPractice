094c03414bbe48b2ae85bf16fa0ec48dbb7e7c34
Kent Nolan <kent.nolan@gmail.com>
2023-05-23 11:19:06 +1200

    Added comments

--- src/Assignment2/Film.java
+++ src/Assignment2/Film.java
@@ -2,33 +2,32 @@ package Assignment2;
 
 public class Film extends DigitalContent {
 
-
     private String cast;
 
-
+    // Constructs a Film object with the given title, publisher, release date, and cast.
     public Film(String title, String publisher, String released, String cast) {
         super(title, publisher, released);
         this.cast = cast;
     }
 
-
+    // Sets the cast of the film.
     public void setCast(String cast) {
         this.cast = cast;
     }
 
-
+    // Returns the cast of the film.
     public String getCast() {
         return cast;
     }
 
-
+    // Returns a string representation of the film, including the cast.
     public String toString() {
         return super.toString() + " Cast: " + cast;
     }
 
+    // Checks if the film matches the given query, including the cast.
     @Override
     public boolean match(String query) {
         return super.match(query) || cast.toLowerCase().contains(query.toLowerCase());
     }
-
 }
