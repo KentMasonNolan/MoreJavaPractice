850afe1829e5e9ff6caf11807029bddff18f9316
kent <kent.nolan@gmail.com>
2023-05-22 14:30:28 +1200

    Week 10 lab 9

---
+++ src/Week10/ComputeProductMenu.java
@@ -0,0 +1,13 @@
+package Week10;
+
+import java.util.Scanner;
+
+public class ComputeProductMenu{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
+
