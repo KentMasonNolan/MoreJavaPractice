94236a80dfe420400ecd5002ea38d243ef5965f6
Kent Nolan <kent.nolan@gmail.com>
2023-05-23 11:20:02 +1200

    Added comments

--- src/Assignment2/StreamingService.java
+++ src/Assignment2/StreamingService.java
@@ -2,26 +2,26 @@ package Assignment2;
 
 import java.util.ArrayList;
 
-
 public class StreamingService {
 
     private ArrayList<DigitalContent> content;
 
+    // Constructs a StreamingService object with an empty list of digital content.
     public StreamingService() {
         content = new ArrayList<>();
     }
 
+    // Adds a digital content item to the streaming service.
     public void add(DigitalContent item) {
         content.add(item);
     }
 
-    public ArrayList match(String query)
-    {
+    // Returns an ArrayList of digital content items that match the given query.
+    public ArrayList match(String query) {
         ArrayList<DigitalContent> matchedItems = new ArrayList<>();
 
-        for(DigitalContent dc : content)
-        {
-            if(dc.match(query))
+        for (DigitalContent dc : content) {
+            if (dc.match(query))
                 matchedItems.add(dc);
         }
 
