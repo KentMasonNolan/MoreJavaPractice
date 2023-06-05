ea688ce66734f46536107057fdfb7e2c976828bc
Kent Nolan <kent.nolan@gmail.com>
2023-06-05 23:02:10 +1200

    Added files and setup for Exam Prac

---
+++ src/Exam_Pracw4/NumberList.java
@@ -0,0 +1,108 @@
+package Exam_Pracw4;
+
+
+import java.util.Random;
+
+public class NumberList {
+
+    private int[] numbers;
+
+    public NumberList() {
+        numbers = new int[10];
+    }
+
+    public NumberList(int r) {
+        numbers = new int[10]; // Initialize the array with 10 elements
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(r); // Generate random numbers between 0 and r-1
+        }
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < 10; i++) {
+            s += numbers[i];
+            s += " ";
+
+        }
+        return s;
+    }
+
+    public int size() {
+        return numbers.length;
+    }
+
+    public boolean isValid(int i) {
+        for (int number : numbers) {
+            if (number == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void update(int i, int value) {
+        if (!isValid(i)) {
+            return;
+        }
+        for (int number : numbers) {
+            if (numbers[number] == i) {
+                numbers[number] = value;
+            }
+        }
+    }
+
+    public int min() {
+        int min = 99;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
+    public int max() {
+        int max = -1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    public int nonZero(){
+        int zeros = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 0){
+                zeros ++;
+            }
+        }
+        return zeros;
+    }
+
+    public double average(){
+        double average = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            average += numbers[i];
+        }
+        return average / numbers.length;
+    }
+
+    public int getNumber(int i){
+
+        for (int j = 0; j < numbers.length; j++) {
+            if (numbers[j] == i){
+                return j;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/Exam_Pracw4/NumberListTest.java
@@ -0,0 +1,33 @@
+package Exam_Pracw4;
+
+public class NumberListTest {
+
+    public static void main(String[] args) {
+
+
+        NumberList Array1 = new NumberList(10);
+
+        System.out.println("List size = " + Array1.size());
+
+        System.out.println("List 1: " + Array1);
+
+        System.out.println("Checking if 7 is in the list: " + Array1.isValid(7));
+
+        System.out.println("If 7 is in the list, we will update to 8");
+        Array1.update(7,8);
+
+        System.out.println("List 1: " + Array1);
+
+        System.out.println("The smallest number is: " + Array1.min());
+        System.out.println("The largest number is: " + Array1.max());
+
+        System.out.println("The number of zeros in the list is: " + Array1.nonZero());
+
+        System.out.println("The average of the array is: " + Array1.average());
+
+        System.out.println("Checking where 8 is in the list. It is in position " + Array1.getNumber(8));
+
+
+
+    }
+}
