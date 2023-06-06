52f179bdc34ea6160152c280dac91c7e0322aea6
Kent Nolan <kent.nolan@gmail.com>
2023-06-07 11:57:53 +1200

    Added files and setup for Exam Prac

--- src/Exam_PracW6/TrafficLight.java
+++ src/Exam_PracW6/TrafficLight.java
@@ -1,4 +1,29 @@
 package Exam_PracW6;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class TrafficLight {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please Type a valid input string.");
+            System.out.println("Your options are: “RED”, “YELLOW” and “GREEN”");
+            String userInput = input.nextLine();
+
+            if (userInput.equalsIgnoreCase("redred")){
+                System.exit(3);
+            }
+
+            for (int i = 0; i < Lights.values().length; i++) {
+                if (userInput.equalsIgnoreCase(Lights.values()[i].name())){
+                    System.out.println(Lights.values()[i]);
+                }
+            }
+        }
+    }
+
 }
