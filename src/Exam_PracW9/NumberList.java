0c48965478957dc4620b6b6cf836372340cf3fec
Kent Nolan <kent.nolan@gmail.com>
2023-06-07 16:32:53 +1200

    Added files and setup for Exam Prac

--- src/Exam_PracW9/NumberList.java
+++ src/Exam_PracW9/NumberList.java
@@ -4,5 +4,39 @@ import java.util.ArrayList;
 
 public class NumberList {
 
-    ArrayList<Integer> list = new ArrayList<>();
+    private ArrayList<Integer> list;
+
+    public NumberList(String input) {
+        list = new ArrayList<>();
+        if (!input.isEmpty()) {
+            String[] numberString = input.split(" ");
+            for (String numberStrings : numberString) {
+                int number = Integer.valueOf(numberStrings);
+                list.add(number);
+            }
+        }
+    }
+
+    public NumberList(ArrayList<Integer> numberInput) {
+        this.list = numberInput;
+    }
+
+    public ArrayList<Integer> getNumbers() {
+        return list;
+    }
+
+    public int sum() {
+        int total = 0;
+        for (Integer lists : list) {
+            total += lists;
+        }
+        return total;
+    }
+
+    public double average() {
+        int total = this.sum();
+        return ((double) total / (double) list.size());
+    }
+
+
 }
