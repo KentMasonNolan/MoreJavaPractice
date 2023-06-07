3c629e037d292834ebb21fc202b75a270a895176
Kent Nolan <kent.nolan@gmail.com>
2023-06-07 13:02:07 +1200

    Added files and setup for Exam Prac

--- src/Exam_PracW6/RugbyScore.java
+++ src/Exam_PracW6/RugbyScore.java
@@ -10,9 +10,18 @@ public class RugbyScore {
         this.teamScore = 0;
     }
 
+    public void score(ScoreAction action){
+        for (int i = 0; i < ScoreAction.values().length; i++) {
+            if (action == ScoreAction.values()[i]){
+                int tempScore = ScoreAction.values()[i].getWorth();
+                this.teamScore += tempScore;
+            }
+        }
+    }
+
     @Override
     public String toString() {
-        return "RugbyScore{" + "teamName='" + teamName + '\'' + ", teamScore=" + teamScore + '}';
+        return "Team Name:" + teamName + " , Score: " + teamScore;
     }
 
     public String getTeamName() {
@@ -29,5 +38,19 @@ public class RugbyScore {
 
     public void setTeamScore(int teamScore) {
         this.teamScore = teamScore;
+    }
+
+    public void incTeamScore(int teamScore) {
+        this.teamScore += teamScore;
+    }
+
+    public static void main(String[] args)
+    {
+        RugbyScore blues = new RugbyScore("Blues");
+        System.out.println("Before they score points: " + blues);
+        blues.score(ScoreAction.TRY);
+        System.out.println("After they score a try: "+blues);
+        blues.score(ScoreAction.DROPGOAL);
+        System.out.println("After they score a dropgoal: " + blues);
     }
 }
--- src/Exam_PracW6/ScoreAction.java
+++ src/Exam_PracW6/ScoreAction.java
@@ -2,12 +2,23 @@ package Exam_PracW6;
 
 public enum ScoreAction {
     TRY(5), CONVERSIONKICK(2), PENALTYKICK(3), DROPGOAL(3);
-        private int points;
+        private int worth;
 
 
-    ScoreAction(int points) {
-        this.points = points;
+    ScoreAction(int worth) {
+        this.worth = worth;
     }
 
+    public int getWorth() {
+        return worth;
+    }
+
+    public void setWorth(int worth) {
+        this.worth = worth;
+    }
+
+    public void ScoreAction(int worth){
+        this.worth = worth;
+    }
 
 }
